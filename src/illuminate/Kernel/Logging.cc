684351dc989daa71c1fe8e2292804b2e198f048d
takooctopus <takooctopus@hotmail.com>
2022-02-25 01:23:26 -0500

    Logging.h/cc

--- src/illuminate/Kernel/Logging.cc
+++ src/illuminate/Kernel/Logging.cc
@@ -16,3 +16,41 @@
 
 static pthread_once_t once_control_ = PTHREAD_ONCE_INIT;
 static AsyncLogging* async_logger_;
+
+std::string Logger::log_file_name_ = "./log/WebServer.log";
+
+void once_init(){
+    async_logger_ = new AsyncLogging(Logger::getLogFileName());
+    async_logger_->start();
+}
+
+void output(const char* msg, int len){
+    pthread_once(&once_control_, once_init);
+    async_logger_->append(msg, len);
+}
+
+Logger::Impl::Impl(const char* file_name, int line)
+    :   stream_(),
+        line_(),
+        base_name_(file_name){
+            formatTime();
+        }
+
+void Logger::Impl::formatTime(){
+    struct timeval tv;
+    time_t time;
+    char str_t[26] = {0};
+    gettimeofday(&tv, nullptr);
+    time = tv.tv_sec;
+    struct tm* p_time = localtime(&time);
+    strftime(str_t, 26, "%Y-%m-%d %H:%M:%S\n", p_time);
+    stream_ << str_t;
+}
+
+Logger::Logger(const char* file_name, int line): impl_(file_name, line){}
+
+Logger::~Logger(){
+    impl_.stream_ << " -- " << impl_.base_name_ << ':' << impl_.line_ << " \n";
+    const LogStream::Buffer& buff(stream().buffer());
+    output(buff.data(), buff.length());
+}
--- src/illuminate/Kernel/Logging.h
+++ src/illuminate/Kernel/Logging.h
@@ -24,6 +24,17 @@ class Logger{
         Impl impl_;
         static std::string log_file_name_;
     public:
+        Logger(const char* file_name, int line);
+        ~Logger();
+        LogStream &stream() {return impl_.stream_; }
+
+        static void setLogFileName(std::string file_name){
+            log_file_name_ = file_name;
+        }
+
+        static std::string getLogFileName(){
+            return log_file_name_;
+        }
 }
 
 #define LOG Logger(__FILE__, __LINE__).stream()
